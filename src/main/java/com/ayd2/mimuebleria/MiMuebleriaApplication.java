245c231626a03f8b82908056aee000ff5eaa4bac
L2AsHdz <leonidas0197@gmail.com>
2024-03-13 21:29:37 -0600

    initial commit

---
+++ src/main/java/com/ayd2/mimuebleria/MiMuebleriaApplication.java
@@ -0,0 +1,13 @@
+package com.ayd2.mimuebleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMuebleriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiMuebleriaApplication.class, args);
+    }
+
+}
